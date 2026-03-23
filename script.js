de02a6aacb1b3334b7b3fa604a6aea6f38aa2e15
ethanmcgill555-eng <ethanmcgill555@gmail.com>
2026-03-23 17:50:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,8 @@ function scrollToContact() {
 
 document.getElementById("contactForm").addEventListener("submit", function(e) {
   e.preventDefault();
-
   const msg = document.getElementById("formMessage");
   msg.textContent = "Thanks! We'll be in touch soon.";
   msg.style.color = "#2563eb";
-
   this.reset();
 });
